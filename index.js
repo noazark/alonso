2bc49825771eb109cbb4209035ac08f5eb763c40
Noah H. Smith <noah@batteredoats.com>
2014-07-17 14:09:08 -0700

    setting unlimited listeners on stdout&err

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var elasticsearchHosts = process.env.ELASTICSEARCH_HOSTS.split(',')
 var logs = process.env.LOGS.split(',')
 var scriptPath = path.join(__dirname, './bin/alonso')
 
+process.stdout.setMaxListeners(0)
+process.stderr.setMaxListeners(0)
+
 logs.forEach(function (log) {
 	var child = forever.start([scriptPath, log], {env: process.env})
 })
